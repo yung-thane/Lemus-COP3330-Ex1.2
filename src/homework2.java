bcd79f6ee297123ec0423ff41609830a055dbddd
Ronald <ronald.cristobal.lemus@outlook.com>
2021-09-11 07:51:05 -0400

    Commit 2

--- src/homework2.java
+++ src/homework2.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class homework2 {
     public static void main(String[] args) {
+        System.out.print("What is the input string?");
+
+        Scanner string = new Scanner(System.in);
+
+        String input = string.nextLine();
+
 
     }
 }
