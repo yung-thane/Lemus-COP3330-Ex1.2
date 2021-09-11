d90f9f60725688321fabe79dceed3c672d414be6
Ronald <ronald.cristobal.lemus@outlook.com>
2021-09-11 09:30:08 -0400

    Commit 2

--- src/homework2.java
+++ src/homework2.java
@@ -2,12 +2,15 @@ import java.util.Scanner;
 
 public class homework2 {
     public static void main(String[] args) {
-        System.out.print("What is the input string?");
+        System.out.print("What is the input string? ");
 
         Scanner string = new Scanner(System.in);
 
         String input = string.nextLine();
 
+        int length = input.length();
+
+        System.out.printf("%s has %d characters.",input,length);
 
     }
 }
